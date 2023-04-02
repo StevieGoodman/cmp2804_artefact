27d8e267a472d85d0c5e4fa48dccb691ef28d920
Stevie Goodman <mail@steviegoodman.com>
2023-04-02 22:13:15 +0100

    Added distraction mechanic
    Signed-off-by: Stevie Goodman <mail@steviegoodman.com>

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -1,5 +1,7 @@
 using cmp2804.Characters.Detection;
 using cmp2804.Characters.States;
+using cmp2804.DistractionMechanic;
+using cmp2804.Math;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UnityEngine;
@@ -9,7 +11,7 @@ namespace cmp2804.Characters
     [RequireComponent(typeof(Patrol), typeof(Chase))]
     [RequireComponent(typeof(Sight), typeof(Investigate))]
     [HideMonoScript]
-    public class EnemyController : SerializedMonoBehaviour
+    public class EnemyController : SerializedMonoBehaviour, IDistractable
     {
         [Title("State", "The current state defining the enemy's behaviour.")] 
         [OdinSerialize, HideLabel]
@@ -24,6 +26,14 @@ namespace cmp2804.Characters
         {
             await State.TickState();
             State.UpdateState();
+        }
+        
+        public void Distract(DistractionSource source)
+        {
+            if (State is Chase) return;
+            var investigate = gameObject.GetComponent<Investigate>();
+            investigate.Target = new Target(source.Origin);
+            State = investigate;
         }
     }
 }
