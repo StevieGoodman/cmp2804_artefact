de1ff605df1d155dfca4ed104512c6afd04885a5
jackh02 <25624184@students.lincoln.ac.uk>
2023-04-03 00:37:15 +0100

    Enemy state indicator completed

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -1,8 +1,10 @@
+using System;
 using cmp2804.Characters.Detection;
 using cmp2804.Characters.States;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace cmp2804.Characters
 {
@@ -15,15 +17,47 @@ namespace cmp2804.Characters
         [OdinSerialize, HideLabel]
         public EnemyState State { get; set; }
 
+        public Canvas enemyStateCanvas;
+        public GameObject enemyStateIndicator;
+        public Sprite enemyStatePatrol;
+        public Sprite enemyStateInvestigate;
+        public Sprite enemyStateChase;
+
+        private Camera _camera;
+        
         private void Awake()
         {
             State = gameObject.GetComponent<Patrol>();
+            enemyStateIndicator.GetComponent<Image>().sprite = enemyStatePatrol;
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            enemyStateCanvas.transform.rotation = _camera.transform.rotation;
         }
 
         public async void FixedUpdate()
         {
             await State.TickState();
             State.UpdateState();
+
+            if (State == gameObject.GetComponent<Patrol>())
+            {
+                enemyStateIndicator.GetComponent<Image>().sprite = enemyStatePatrol;
+            }
+            else if (State == gameObject.GetComponent<Investigate>())
+            {
+                enemyStateIndicator.GetComponent<Image>().sprite = enemyStateInvestigate;
+            }
+            else if (State == gameObject.GetComponent<Chase>())
+            {
+                enemyStateIndicator.GetComponent<Image>().sprite = enemyStateChase;
+            }
         }
     }
 }
